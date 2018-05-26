1470c18db7d2a6484cc92aa1fccee9b236bb8e3e
Your Name <gs.pavan00715@gmail.com>
2018-05-26 23:00:38 +0530

    new update

--- src/sample.java
+++ src/sample.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-	System.out.println ("Hello Banglore");
+	System.out.println ("Hello Banglore City");
     }
 }
